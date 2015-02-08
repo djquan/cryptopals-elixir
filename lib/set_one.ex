16310eb4879112a97a6b5d10357084457c8508c7
Daniel Quan <djquan@gmail.com>
2015-02-07 23:16:12 -0600

    WIP - Begin fixed xor

--- lib/set_one.ex
+++ lib/set_one.ex
@@ -1,7 +1,22 @@
 defmodule SetOne do
+  use Bitwise;
+
   def hex_to_base64(input) do
     String.upcase(input) |> 
       Base.decode16! |>
       Base.encode64
   end
+
+  def my_hex_xor(a, b) do
+    my_xor(Base.decode16!(a), Base.decode16!(b)) |>
+      to_string |>
+      Base.encode16
+  end
+
+  def my_xor(a,b) do
+    for x <- b, do: x
+  end
+
+  # <<r::8, g::8, b::8 <- pixels >>, do: {r, g, b}
+
 end
--- test/set_one_test.exs
+++ test/set_one_test.exs
@@ -6,4 +6,11 @@ defmodule SetOneTest do
     actual = SetOne.hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
     assert expected == actual
   end
+
+  test "can XOR two equal length buffers" do
+    a = "1c0111001f010100061a024b53535009181c" |> String.upcase
+    b = "686974207468652062756c6c277320657965" |> String.upcase
+    expected = "746865206b696420646f6e277420706c6179" |> String.upcase
+    assert expected == SetOne.my_hex_xor(a,b)
+  end
 end
