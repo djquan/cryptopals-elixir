128a4b3b391b4b31be4003818f35e172dce25fc3
Daniel Quan <djquan@gmail.com>
2015-07-03 15:10:45 -0700

    Finish challenge eleven, encryption oracle #1

---
+++ lib/set_2/challenge_11.ex
@@ -0,0 +1,57 @@
+defmodule SetTwo.ChallengeEleven do
+  import SetTwo.ChallengeTen
+
+  @doc """
+  Pads a plaintext with a random amount of bytes before and after
+  Encrypts a given plaintext with aes_128_ecb OR aes_128_cbc
+  Uses a random cipher
+  http://cryptopals.com/sets/2/challenges/11
+  """
+  def encryption_oracle(plaintext) do
+    plaintext
+    |> pad_random
+    |> encrypt_random_ecb_cbc
+  end
+
+  def encrypt_random_ecb_cbc(plaintext) do
+    cipher = generate_random_aes_key
+    if (:random.uniform(2) == 1) do
+      {"ecb", encrypt_aes_128_ecb(plaintext, cipher) }
+    else
+      {"cbc", encrypt_aes_128_cbc(plaintext, cipher, generate_random_aes_key)}
+    end
+  end
+
+  @doc """
+  Determines if a given ciphertext is encrypted with ecb or cbc.
+  Leverages Challenge Eight to do so.
+  http://cryptopals.com/sets/2/challenges/11
+  """
+  def discover_type(ciphertext) do
+    if SetOne.ChallengeEight.encrypted_aes_ecb?(ciphertext), do: "ecb", else: "cbc"
+  end
+
+  @doc """
+  Under the hood, have the function append 5-10 bytes (count chosen randomly) before the plaintext and 5-10 bytes after the plaintext.
+
+  ### Examples
+  iex> Regex.match?(~r/.{5,10}plaintext.{5,10}/s, SetTwo.ChallengeEleven.pad_random("plaintext"))
+  true
+  """
+  def pad_random(plaintext) do
+    generate_random_bytes(:random.uniform(6) + 4)
+    <> plaintext
+    <> generate_random_bytes(:random.uniform(6) + 4)
+  end
+
+  @doc """
+  Generates a random AES key
+  """
+  def generate_random_aes_key, do: generate_random_bytes(16)
+
+  defp generate_random_bytes(num) do
+    Stream.repeatedly(fn -> :random.uniform(255) end)
+    |> Enum.take(num)
+    |> :binary.list_to_bin
+  end
+end
---
+++ test/set_2/challenge_11_test.exs
@@ -0,0 +1,105 @@
+defmodule SetTwo.ChallengeElevenTest do
+  use ExUnit.Case, async: true
+  import SetTwo.ChallengeEleven
+  doctest SetTwo.ChallengeEleven
+
+  setup do
+    :random.seed(:erlang.now)
+    :ok
+  end
+
+  test "generates a random AES key" do
+    assert byte_size(generate_random_aes_key) == 16
+  end
+
+  test "encrypts either with cbc or ecb" do
+    {encryption_type, _ciphertext} = encrypt_random_ecb_cbc(bad_blood)
+    assert Regex.match?(~r/ecb|cbc/, encryption_type)
+  end
+
+  test "determines whether a ciphertext was encrypted with cbc or ecb" do
+    {encryption_type, ciphertext} = encryption_oracle(bad_blood)
+    assert encryption_type == discover_type(ciphertext)
+  end
+
+  def bad_blood do
+"""
+Cause, baby, now we got bad blood
+You know it used to be mad love
+So take a look what youve done
+Cause, baby, now we got bad blood
+Hey
+Now we got problems
+And I dont think we can solve them
+You made a really deep cut
+And, baby, now we got bad blood
+Hey
+
+Did you have to do this? I was thinking that you could be trusted
+Did you have to ruin what was shiny? Now its all rusted
+Did you have to hit me, where Im weak? Baby, I couldnt breathe
+And rub it in so deep, salt in the wound like youre laughing right at me
+
+Oh, its so sad to think about the good times, you and I
+
+Cause, baby, now we got bad blood
+You know it used to be mad love
+So take a look what youve done
+Cause, baby, now we got bad blood
+Hey
+Now we got problems
+And I dont think we can solve them
+You made a really deep cut
+And, baby, now we got bad blood
+Hey
+
+Did you think wed be fine? Still got scars on my back from your knife
+So dont think its in the past, these kinda wounds they last and they last.
+Now did you think it all through? All these things will catch up to you
+And time can heal but this wont, so if youre coming my way, just dont
+
+Oh, its so sad to think about the good times, you and I
+
+Cause, baby, now we got bad blood
+You know it used to be mad love
+So take a look what youve done
+Cause, baby, now we got bad blood
+Hey
+Now we got problems
+And I dont think we can solve them
+You made a really deep cut
+And, baby, now we got bad blood
+Hey
+
+Band-aids dont fix bullet holes
+You say sorry just for show
+If you live like that, you live with ghosts (ghosts)
+Band-aids dont fix bullet holes (hey)
+You say sorry just for show (hey)
+If you live like that, you live with ghosts (hey)
+If you love like that blood runs cold
+
+Cause, baby, now we got bad blood
+You know it used to be mad love (mad love)
+So take a look what youve done
+Cause, baby, now we got bad blood
+Hey
+Now we got problems
+And I dont think we can solve them (think we can solve them)
+You made a really deep cut
+And, baby, now we got bad blood
+Hey
+
+Cause, baby, now we got bad blood
+You know it used to be mad love
+So take a look what youve done (look what youve done)
+Cause, baby, now we got bad blood
+Hey
+Now we got problems
+And I dont think we can solve them
+You made a really deep cut
+And, baby, now we got bad blood
+Hey
+"""
+  end
+end
