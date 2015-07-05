62b6795b70de947a42cc85a894ba04b3c865a0c3
Daniel Quan <djquan@gmail.com>
2015-07-05 12:15:43 -0700

    Add documentation and clean up code a little

--- lib/set_2/challenge_12.ex
+++ lib/set_2/challenge_12.ex
@@ -2,6 +2,10 @@ defmodule SetTwo.ChallengeTwelve do
   import SetTwo.ChallengeTen
   @key SetTwo.ChallengeEleven.generate_random_aes_key
 
+  @doc """
+  Cracks and discovers the secret encrypted with encryption_oracle_2
+  http://cryptopals.com/sets/2/challenges/12/
+  """
   def crack_code do
     ciphertext = encryption_oracle_2("")
     if SetOne.ChallengeEight.encrypted_aes_ecb?(ciphertext) do
@@ -9,7 +13,6 @@ defmodule SetTwo.ChallengeTwelve do
     end
 
     block_size = determine_block_size(ciphertext)
-    input_block = :binary.copy("A", block_size - 1)
 
     (1..num_blocks(ciphertext, block_size))
     |> Enum.reduce("", fn(x, acc) ->
@@ -20,6 +23,11 @@ defmodule SetTwo.ChallengeTwelve do
 
   def brute_force_decrypt(block_size, known) when byte_size(known) == block_size, do: ""
 
+  @doc """
+  Brute force decrypts to discover one byte of plaintext.
+  input_block is a mixture of A's and what is already known
+  The total size of input_block is one short of the byte to be discovered.
+  """
   def brute_force_decrypt(block_size, known) do
     input_block = :binary.copy("A", block_size - 1 - byte_size(known))
     sample = Kernel.binary_part(encryption_oracle_2(input_block), 0, block_size)
@@ -51,17 +59,25 @@ defmodule SetTwo.ChallengeTwelve do
     |> hd
   end
 
+  @doc """
+  An encryption oracle that takes plaintext, pads it with a secret
+  It then encrypts it with a constant, random key.
+  """
+  def encryption_oracle_2(plaintext) do
+    plaintext
+    |> pad_secret
+    |> encrypt_aes_128_ecb(@key)
+  end
+
+  @doc """
+  Helper function
+  Counts the number of blocks in a ciphertext when given the block size
+  """
   def num_blocks(ciphertext, block_size) do
     ciphertext
     |> :binary.bin_to_list
     |> Enum.chunk(block_size)
     |> length
-  end
-
-  def encryption_oracle_2(plaintext) do
-    plaintext
-    |> pad_secret
-    |> encrypt_aes_128_ecb(@key)
   end
 
   defp pad_secret(plaintext), do: plaintext <> secret
