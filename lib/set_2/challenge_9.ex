0d92ea7309000550b7c319660dd9a7f951356f0b
Dan Quan <daniel@quan.io>
2016-09-07 22:23:46 -0700

    Add doctest

--- lib/set_2/challenge_9.ex
+++ lib/set_2/challenge_9.ex
@@ -2,6 +2,10 @@ defmodule SetTwo.ChallengeNine do
   @doc """
   Pads a block with a supplied length with <<4>>
   http://cryptopals.com/sets/2/challenges/9/
+
+  ### Examples
+    iex> SetTwo.ChallengeNine.pad("hello", 6)
+    "hello\x04"
   """
   @spec pad(binary, pos_integer) :: binary
   def pad(block, pad_length), do: String.ljust(block, pad_length, 4)
